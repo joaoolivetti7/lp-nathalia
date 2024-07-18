131bdde18fef5eec97765a72c4b9c32c7f16b8c5
João Olivetti <108378767+joaoolivetti7@users.noreply.github.com>
2024-07-18 10:48:03 -0300

    media query

--- scripts/script.js
+++ scripts/script.js
@@ -15,4 +15,30 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }).mount();
 
+    const icon = document.querySelector(".menu-icon");
+
+  let isOpen = false;
+  let timeout;
+
+  icon.addEventListener("click", () => {
+    const menu = document.getElementById("header-list");
+    if (!isOpen) {
+      menu.classList.add("ascending");
+      menu.classList.remove("descending");
+      menu.style.display = "flex";
+      isOpen = true;
+    } else {
+      menu.classList.add("descending");
+      menu.classList.remove("ascending");
+      
+      if (!timeout) {
+        timeout = setTimeout(() => {
+          menu.style.display = "none";
+          timeout = undefined;
+        }, 180);
+      }
+
+      isOpen = false;
+    }
+  });
 });
